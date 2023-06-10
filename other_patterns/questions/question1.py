7db57d9465a0dacb97126ba57f81ef2db7e63e98
Sujeet B <12060698+sujeet-bhalerao@users.noreply.github.com>
2023-06-09 19:20:58 -0500

    questions

--- other_patterns/questions/question1.py
+++ other_patterns/questions/question1.py
@@ -3,7 +3,6 @@ from sympy import primerange
 import os
 import math
 
-
 def fractional_part(x):
     return x - math.floor(x)
 
@@ -12,13 +11,15 @@ def number_of_mh_pairs_condition1(k):
     h_limit = (k - 1) // 2
     count_satisfying = 0
     count_not_satisfying = 0
+    satisfying_pairs = []
     for m in range(1, m_limit):
         for h in range(1, h_limit + 1):
             if fractional_part(2 * m * h / k) >= fractional_part(h / k) and fractional_part(2 * m * h / k) > 0.5:
                 count_satisfying += 1
+                satisfying_pairs.append((m, h))
             else:
                 count_not_satisfying += 1
-    return count_satisfying, count_not_satisfying
+    return count_satisfying, count_not_satisfying, satisfying_pairs
 
 k_start = 3
 k_end = 501
@@ -30,7 +31,11 @@ output_file_path = os.path.join(script_dir, "q1_pairs_count.txt")
 
 with open(output_file_path, "w") as output_file:
     output_file.write("k\tSatisfying Condition 1\tNot Satisfying Condition 1\n")
-    for k, (satisfying_count, not_satisfying_count) in zip(odd_primes, results):
+    for k, (satisfying_count, not_satisfying_count, satisfying_pairs) in zip(odd_primes, results):
         output_file.write(f"{k}\t{satisfying_count}\t{not_satisfying_count}\n")
+        output_file.write("Satisfying Pairs: ")
+        for pair in satisfying_pairs:
+            output_file.write(f"{pair} ")
+        output_file.write("\n")
 
 print("Results saved to", output_file_path)
--- other_patterns/questions/question2.py
+++ other_patterns/questions/question2.py
@@ -3,23 +3,23 @@ from sympy import primerange
 import os
 import math
 
-
 def fractional_part(x):
     return x - math.floor(x)
-
 
 def number_of_mh_pairs_condition2(k):
     m_limit = (k + 1) // 4
     h_limit = (k - 1) // 2
     count_satisfying = 0
     count_not_satisfying = 0
+    satisfying_pairs = []
     for m in range(1, m_limit):
         for h in range(1, h_limit + 1):
             if fractional_part(2 * m * h / k) > 0.5:
                 count_satisfying += 1
+                satisfying_pairs.append((m, h))
             else:
                 count_not_satisfying += 1
-    return count_satisfying, count_not_satisfying
+    return count_satisfying, count_not_satisfying, satisfying_pairs
 
 k_start = 3
 k_end = 501
@@ -30,8 +30,12 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 output_file_path = os.path.join(script_dir, "q2_pairs_count.txt")
 
 with open(output_file_path, "w") as output_file:
-    output_file.write(" k\tsatisfy ineq\tdon't satisfy ineq\n")
-    for k, (satisfying_count, not_satisfying_count) in zip(odd_primes, results):
+    output_file.write("k\tSatisfying Condition 2\tNot Satisfying Condition 2\n")
+    for k, (satisfying_count, not_satisfying_count, satisfying_pairs) in zip(odd_primes, results):
         output_file.write(f"{k}\t{satisfying_count}\t{not_satisfying_count}\n")
+        output_file.write("Satisfying Pairs: ")
+        for pair in satisfying_pairs:
+            output_file.write(f"{pair} ")
+        output_file.write("\n")
 
 print("Results saved to", output_file_path)
